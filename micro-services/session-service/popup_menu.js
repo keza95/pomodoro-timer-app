e1dd505d4ea4d53c0ea2b72cdb7fbd81f3d8f175
keza95 <270185550@yoobeestudent.ac.nz>
2023-06-16 15:03:21 +1200

    Feature: Popup Menu

--- micro-services/session-service/popup_menu.js
+++ micro-services/session-service/popup_menu.js
@@ -1,65 +1,65 @@
-    $(document).ready(function () {
-      var menu = $('#popup-menu');
-      var openBtn = $('#open-menu-btn');
-      var customizationBtn = $('#customization-btn');
-      var viewBtn = $('#view-btn');
-      var logoutBtn = $('#logout-btn');
-      var loginBtn = $('#login-btn');
-      var sectionTitle = $('#section-title');
-      var customizationContent = $('#customization-content');
-      var viewContent = $('#view-content');
+$(document).ready(function () {
+    var hamburgerIcon = $('#hamburger-icon');
+    var popupMenu = $('#popup-menu');
+    var customizationBtn = $('#customization-btn');
+    var viewBtn = $('#view-btn');
+    var logoutBtn = $('#logout-btn');
+    var loginBtn = $('#login-btn');
+    var sectionTitle = $('#section-title');
+    var customizationContent = $('#customization-content');
+    var viewContent = $('#view-content');
 
-      // Function to open the menu
-      function openMenu() {
-        menu.show();
-      }
+    // Function to open the menu
+    function openMenu() {
+        popupMenu.show();
+    }
 
-      // Function to close the menu
-      function closeMenu() {
-        menu.hide();
-      }
+    // Function to close the menu
+    function closeMenu() {
+        popupMenu.hide();
+    }
 
-      // Function to switch to the Customization section
-      function switchToCustomization() {
+    // Function to switch to customization section
+    function switchToCustomization() {
         sectionTitle.text('Customization');
         customizationContent.addClass('active');
         viewContent.removeClass('active');
-      }
+    }
 
-      // Function to switch to the View section
-      function switchToView() {
+    // Function to switch to view section
+    function switchToView() {
         sectionTitle.text('View');
         customizationContent.removeClass('active');
         viewContent.addClass('active');
-      }
+    }
 
-      // Function to handle login/logout
-      function handleAuthentication(loggedIn) {
+    // Function to handle login/logout
+    function handleAuthentication(loggedIn) {
         if (loggedIn) {
-          logoutBtn.show();
-          loginBtn.hide();
+            logoutBtn.show();
+            loginBtn.hide();
         } else {
-          logoutBtn.hide();
-          loginBtn.show();
+            logoutBtn.hide();
+            loginBtn.show();
         }
-      }
+    }
 
-      // Initially hide the logout button
-      logoutBtn.hide();
+    // Initially hide the logout button
+    logoutBtn.hide();
 
-      // Attach event handlers
-      openBtn.click(openMenu);
-      menu.click(function (event) {
-        if (event.target === menu[0]) {
-          closeMenu();
+    // Attach event handlers
+    hamburgerIcon.click(openMenu);
+    popupMenu.click(function (event) {
+        if (event.target === popupMenu[0]) {
+            closeMenu();
         }
-      });
-      customizationBtn.click(switchToCustomization);
-      viewBtn.click(switchToView);
-      logoutBtn.click(function () {
-        handleAuthentication(false);
-      });
-      loginBtn.click(function () {
-        handleAuthentication(true);
-      });
     });
+    customizationBtn.click(switchToCustomization);
+    viewBtn.click(switchToView);
+    logoutBtn.click(function () {
+        handleAuthentication(false);
+    });
+    loginBtn.click(function () {
+        handleAuthentication(true);
+    });
+});
