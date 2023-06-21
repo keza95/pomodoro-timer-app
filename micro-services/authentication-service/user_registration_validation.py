ce975d244a1131411ddd55a9c014725d260820ab
keza95 <270185550@yoobeestudent.ac.nz>
2023-06-21 15:52:04 +1200

    New: Settings UI

--- micro-services/authentication-service/user_registration_validation.py
+++ micro-services/authentication-service/user_registration_validation.py
@@ -10,9 +10,20 @@ def register_user():
     email = data['email']
     password = data['password']
 
+    # TODO: Once completed create new error_handling.py file and refactor code
+    error = {
+        'error_id': "PDA-001",  # PDA = Pomodoro Timer Application
+        'error_message': "Invalid input for username, username should be a string"
+    }
+
+    error_username = {
+        'error_id': "PDA-002",  # PDA = Pomodoro Timer Application
+        'error_message': "Invalid input for username, username should be a string between 5 - 20 characters"
+    }
+
     # Checks the length of the password
     if len(user_name) < 5 or len(user_name) > 20:
-        return jsonify({'message': 'Username must be at between 5 and 20 characters to be valid'}), 400
+        return jsonify({'error': error}), error['error_id']
 
     # Validates email being registered
     if '@' not in email or '.' not in email:
@@ -23,4 +34,3 @@ def register_user():
         return jsonify({'message': 'Pass word must be at least 8 characters long to be valid'}), 400
 
     return jsonify({'message': 'User registered successfully'}), 201
-
